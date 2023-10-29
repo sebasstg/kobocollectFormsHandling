c3e741f3af414852663ea4744e576f82e9ff1c50
sebasstg <sebassalazart@gmail.com>
2023-10-29 09:49:21 -0600

    teting

--- functions.js
+++ functions.js
@@ -1,3 +1,29 @@
+function getEnketoViewUlr(formid, submissionId, username, password) {
+    // code to be executed
+    // return statement (optional)
+    const apiUrl = 'https://kobo.unhcr.org/api/v2/assets/' + formid + '/data/' + submissionId + '/enketo/view/?return_url=false'; // Replace with your API endpoint URL
+    const headers = new Headers();
+    console.log(username);
+    console.log(password);
+    headers.set('Authorization', 'Basic ' + btoa(username + ":" + password));
+    fetch(apiUrl,
+        {
+            method: 'GET',
+            headers: headers,
+            mode: 'no-cors', // <---
+            cache: 'default'
+        }
+    )
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            const formUrl = data.url;
+            console.log(formUrl);
+            return formUrl
+        })
+        .catch(error => console.error(error));
+};
+
 // JavaScript text
 // JavaScript code to extract and display the parameter from the URL
 const urlParams = new URLSearchParams(window.location.search);
@@ -27,47 +53,8 @@ window.onload = function () {
         let password = document.getElementById('password').value;
         username = 'elsalvador_kobo';
         password = 'KoBoElSalvador';
-
-        // Make an API call with Basic Authentication headers
-        const apiUrl = 'https://kobo.unhcr.org/api/v2/assets/aPJeBGZLjjJwgLZaYZQtsm/data/35239549/enketo/view/?return_url=false'; // Replace with your API endpoint URL
-        const headers = new Headers();
-        console.log(username);
-        console.log(password);
-
-        headers.set('Authorization', 'Basic ' + btoa(username + ":" + password));
-
-
-
-
-        fetch(apiUrl, {
-            method: 'GET',
-            headers: headers,
-            mode: 'no-cors', // <---
-            cache: 'default'
-        })
-            .then(response => {
-                console.log(response.ok);
-                console.log(response.type);
-                console.log(response.headers);
-
-                console.log('response::::')
-                    console.log(response);
-                }
-            )
-            /*
-            .then(response => response.json())
-            .then(data => {
-                // Handle the JSON response with two fields (e.g., data.field1, data.field2)
-                console.log(data);
-            })
-
-             */
-            .catch(error => {
-                console.log(error);
-                alert(error);
-                console.error('Error:', error);
-
-            });
+        enketoUrl = getEnketoViewUlr('aPJeBGZLjjJwgLZaYZQtsm', '35239549', username, password);
+        console.log('url: ' + enketoUrl)
     });
 };
 
